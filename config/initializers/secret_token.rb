1ccdde797abaf4ed0f2a24bd9d4ecd60c4448251
chandru-1987 <mail2chandru24@gmail.com>
2016-04-07 12:32:38 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capdeploy::Application.config.secret_token = '39d372b02f0af8e4577678d01445388cedc34454964a71c3965161dfe31d8772c0ab54f119143034f81b58f9cd9ca80c227d58aff9a2491dd7379215040f1499'
